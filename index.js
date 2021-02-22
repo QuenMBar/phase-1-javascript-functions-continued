99f4db427bc9e7e0f1b91544342974ea322fd918
QuenMBar <Qbeast360@gmail.com>
2021-02-22 14:43:09 -0800

    commit tree 1dd58b52b1fae53a0a3e2679e0d3275f7c57ddb5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`;
+}
+
+let mondayWork = (location = "go to the office") => {
+    return `This Monday, I will ${location}.`;
+}
+
+function wrapAdjective(surounder = '*') {
+    return function (adj) {
+        return `You are ${surounder}${adj}${surounder}!`;
+    }
+}
